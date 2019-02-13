1c5dbe9e6cffff1afbdc9bec2b96b9259697ec8e
cecil john tantay <cjbtantay@gmail.com>
2019-02-13 14:13:32 -0800

    create users express router

---
+++ data/routers/postRouter.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const db = require('../helpers/postDb');
+const router = express.Router();
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
-
+const usersRouter = require('./data/routers/usersRouter');
+const cors = require('cors');
 const server = express();
 
-server.use('/api/users');
+server.use(cors());
+server.use(express.json());
+server.use('/api/users', usersRouter);
+
+// server.use('/api/users');
 
 module.exports = server;
