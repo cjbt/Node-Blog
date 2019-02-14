2aec524ed0b636c7dd429a0d958dab47e098771a
cecil john tantay <cjbtantay@gmail.com>
2019-02-13 23:58:33 -0800

    add material ui

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+});
 
 const User = props => {
+  const { classes, user } = props;
   return (
     <>
-      <h3>{props.user.name}</h3>
+      <Paper className={classes.root} elevation={1}>
+        <Typography variant='h5' component='h3'>
+          {user.name}
+        </Typography>
+        <Typography component='p'>
+          Paper can be used to build surface or other elements for your
+          application.
+        </Typography>
+      </Paper>
     </>
   );
 };
 
-export default User;
+export default withStyles(styles)(User);
